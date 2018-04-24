2dbd9ef6fa01d2823a7d3e064c2c0319b87709ac
Lming1 <spear131121@gmail.com>
2018-04-24 14:59:21 +0900

    0424 study

---
+++ src/main/java/JdbcContext.java
@@ -0,0 +1,39 @@
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void workWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = this.dataSource.getConnection();
+            preparedStatement = statementStrategy.makePreparedStatement(connection);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+
+                }
+            }
+        }
+    }
+}
--- src/main/java/UserDao.java
+++ src/main/java/UserDao.java
@@ -4,18 +4,20 @@ import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
+    private JdbcContext jdbcContext;
     private DataSource dataSource;
-
-//    public UserDao(ConnectionMaker dataSource) {
+    //    public UserDao(ConnectionMaker dataSource) {
 //        this.dataSource = dataSource;
 //    }
 //
-//    public void setDataSource(ConnectionMaker dataSource) {
-//        this.dataSource = dataSource;
-//    }
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+    public void setJdbcContext(JdbcContext jdbcContext){
+        this.jdbcContext = jdbcContext;
+    }
+
+
 
     public User get(String id) throws ClassNotFoundException, SQLException {
         Connection connection = dataSource.getConnection();
@@ -44,37 +46,15 @@ public class UserDao {
 
 
     public void add(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-            preparedStatement.setString(1, user.getId());
-            preparedStatement.setString(2, user.getName());
-            preparedStatement.setString(3, user.getPassword());
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException e){
-            throw e;
-        }
-        finally {
-            if (preparedStatement != null){
-                try {
-                    preparedStatement.close();
-                }catch (SQLException e){
-
+        this.jdbcContext.workWithStatementStrategy(
+                connection -> {
+                    PreparedStatement preparedStatement = connection.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+                    preparedStatement.setString(1, user.getId());
+                    preparedStatement.setString(2, user.getName());
+                    preparedStatement.setString(3, user.getPassword());
+                    return preparedStatement;
                 }
-            }
-            if (preparedStatement != null){
-                try {
-                    connection.close();
-                } catch (SQLException e){
-
-                }
-            }
-
-        }
+        );
     }
 
 
@@ -82,34 +62,13 @@ public class UserDao {
 
     public void deleteAll() throws SQLException {
         // 클라이언트 코드
-        StatementStrategy statementStrategy = new DeleteAllStatementStrategy();
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makePreparedStatement(connection);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (preparedStatement != null){
-                try {
-                    preparedStatement.close();
-                }catch (SQLException e){
-
+        this.jdbcContext.workWithStatementStrategy(connection -> {
+                    PreparedStatement preparedStatement = connection.prepareStatement("delete from users");
+                    return preparedStatement;
                 }
-            }
-            if (connection != null){
-                try {
-                    connection.close();
-                } catch (SQLException e){
-
-                }
-            }
-        }
-
-
+        );
     }
+
 
 
     public int getCount() throws SQLException {
